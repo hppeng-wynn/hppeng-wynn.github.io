a4743898f53152b6c0be0ed020895e30bca43b8e
b <a>
2021-01-09 17:35:32 -0600

    Style hack, version bump

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ console.log(url_tag);
  * END testing section
  */
 
-const BUILD_VERSION = "2.9";
+const BUILD_VERSION = "3.0";
 
 document.getElementById("header").textContent = "Wynn build calculator "+BUILD_VERSION+" (db version "+DB_VERSION+")";
 
