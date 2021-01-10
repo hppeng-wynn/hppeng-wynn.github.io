15dee0eef62613c01b4d655be2fc627cced9f246
b <a>
2021-01-10 16:48:37 -0600

    Change id display a bit, add sprint into item json, bump version

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ console.log(url_tag);
  * END testing section
  */
 
-const BUILD_VERSION = "4.6";
+const BUILD_VERSION = "5.0";
 
 document.getElementById("header").textContent = "Wynn build calculator "+BUILD_VERSION+" (db version "+DB_VERSION+")";
 
