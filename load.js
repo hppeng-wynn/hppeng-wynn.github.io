940f4368f4ab62564a090193ee42d044fa0e6070
b <a>
2021-01-10 07:00:57 -0600

    Fix string vs integer values in parsed def/hp

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 10;
+const DB_VERSION = 11;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
 let db;
--- test.js
+++ test.js
@@ -11,7 +11,7 @@ console.log(url_tag);
  * END testing section
  */
 
-const BUILD_VERSION = "4.4";
+const BUILD_VERSION = "4.5";
 
 document.getElementById("header").textContent = "Wynn build calculator "+BUILD_VERSION+" (db version "+DB_VERSION+")";
 
