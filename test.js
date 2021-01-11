76367a065aa58fd6c3be41b473694148811fac49
b <a>
2021-01-10 19:42:09 -0600

    Misc. UI fixes, move skillpoint summary up to top of page

--- test.js
+++ test.js
@@ -157,7 +157,12 @@ function init() {
     for (const item of items) {
         itemLists.get(item.type).push(item.displayName);
         itemMap.set(item.displayName, item);
-        idMap.set(item.id, item.displayName);
+        if (noneItems.includes(item)) {
+            idMap.set(item.id, "");
+        }
+        else {
+            idMap.set(item.id, item.displayName);
+        }
     }
     
     for (const armorType of armorTypes) {
@@ -450,7 +455,7 @@ function calculateBuildStats() {
     let summarybox = document.getElementById("summary-box");
     summarybox.textContent = "";
     let skpSummary = document.createElement("p");
-    skpSummary.textContent = "Summary: Assigned "+player_build.assigned_skillpoints+" skillpoints.";
+    skpSummary.textContent = "Summary: Assigned "+player_build.assigned_skillpoints+" skillpoints. Total: ( " + player_build.total_skillpoints.join(" | ") + " )";
     skpSummary.classList.add("itemp");
     summarybox.append(skpSummary);
     if(player_build.assigned_skillpoints > levelToSkillPoints(player_build.level)){
