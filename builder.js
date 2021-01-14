cbcca785b764ba68cc9f61a7f6685cc2c363af93
dr-carlos <dr_steven_carlos@outlook.com>
2021-01-14 12:48:28 +1030

    Revert "Merge pull request #1 from hppeng-wynn/master"
    This reverts commit 5169c9a9346e98ead90db989ed53085c54f62928, reversing
    changes made to 0e57e0fed6ec02975550b5e1e33fdd84be41abce.

--- builder.js
+++ builder.js
@@ -11,7 +11,7 @@ console.log(url_tag);
  * END testing section
  */
 
-const BUILD_VERSION = "6.8.6";
+const BUILD_VERSION = "6.8.4";
 
 function setTitle() {
     document.getElementById("header").textContent = "WynnBuilder version "+BUILD_VERSION+" (db version "+DB_VERSION+")";
