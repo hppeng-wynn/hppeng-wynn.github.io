09d4dc47dc4cdb19123a3927a36fce7c030f10a9
b <a>
2021-01-20 06:26:48 -0600

    Bump version

--- builder.js
+++ builder.js
@@ -3,7 +3,7 @@ const url_tag = location.hash.slice(1);
 console.log(url_base);
 console.log(url_tag);
 
-const BUILD_VERSION = "6.9.7";
+const BUILD_VERSION = "6.9.8";
 
 function setTitle() {
     let text;
