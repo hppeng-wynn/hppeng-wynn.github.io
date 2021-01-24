e9f8da4dc8815d4cca469787ed7b024ee974c22b
b <a>
2021-01-24 04:09:59 -0600

    WynnAtlas v0

--- items.js
+++ items.js
@@ -15,13 +15,28 @@ function displayItems(items_copy) {
     }
 }
 
+function doItemSearch() {
+    window.scrollTo(0, 0);
+    let input_json = document.getElementById("query-json").value;
+    let input = JSON.parse(input_json);
+    let items_copy = items.slice();
+    document.getElementById("main").textContent = "";
+    for (const _query of input) {
+        const query = queryTypeMap.get(_query.queryType)(_query.value);
+        items_copy = applyQuery(items_copy, query);
+    }
+    document.getElementById("summary").textContent = items_copy.length + " results."
+    displayItems(items_copy);
+}
+
 function init() {
+    return;
     let items_copy = items.slice();
     //let query = new NameQuery("Bob's");
-    let query1 = new IdQuery("poison");
+    let query1 = new IdQuery("sdRaw");
     items_copy = applyQuery(items_copy, query1);
 
-    let query2 = new TypeQuery("boots");
+    let query2 = new TypeQuery("helmet");
     items_copy = applyQuery(items_copy, query2);
 
     displayItems(items_copy);
--- query.js
+++ query.js
@@ -1,25 +1,21 @@
+let queryTypeMap = new Map();
 
 class NameQuery {
-    constructor(string) {
-        this.queryString = string;
-    }
+    constructor(string) { this.queryString = string.toLowerCase(); }
 
     filter(item) {
         if (item.remapID === undefined) {
-            return (item.displayName.includes(this.queryString));
+            return (item.displayName.toLowerCase().includes(this.queryString));
         }
         return false;
     }
 
-    compare(a, b) {
-        return a < b;
-    }
+    compare(a, b) { return a < b; }
 }
+queryTypeMap.set("name", function(s) { return new NameQuery(s); } );
 
 class TypeQuery {
-    constructor(type) {
-        this.type = type;
-    }
+    constructor(type) { this.type = type; }
 
     filter(item) {
         if (item.remapID === undefined) {
@@ -28,10 +24,23 @@ class TypeQuery {
         return false;
     }
 
-    compare(a, b) {
-        return a < b;
-    }
+    compare(a, b) { return a < b; }
 }
+queryTypeMap.set("type", function(s) { return new TypeQuery(s); } );
+
+class CategoryQuery {
+    constructor(category) { this.category = category; }
+
+    filter(item) {
+        if (item.remapID === undefined) {
+            return (item.category === this.category);
+        }
+        return false;
+    }
+
+    compare(a, b) { return a < b; }
+}
+queryTypeMap.set("category", function(s) { return new CategoryQuery(s); } );
 
 class IdQuery {
     constructor(id) {
@@ -45,3 +54,4 @@ class IdQuery {
         return (this.id in item) && (item[this.id]);
     }
 }
+queryTypeMap.set("stat", function(s) { return new IdQuery(s); } );
