cecbf9485056164e09a9595d781665a02b9716b9
b <a>
2021-01-26 03:59:12 -0600

    Fix issue with cinfras tshirt

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 29;
+const DB_VERSION = 30;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
 let db;
