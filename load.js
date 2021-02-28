7225d90fe1088681707f1a6719571d8a91e45445
b <a>
2021-02-28 10:07:22 -0800

    Add new items

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 34;
+const DB_VERSION = 35;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
 let db;
