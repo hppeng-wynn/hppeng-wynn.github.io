e05adb14c74d1ae71a05718460a7859f779e060e
b <a>
2021-05-17 15:29:00 -0700

    Update items from api

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 42;
+const DB_VERSION = 43;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
