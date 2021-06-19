6247ffd7fb4b4b083214a09c9748704fff7f73a0
b <a>
2021-06-19 00:58:16 -0700

    Fix non-rollable items

--- load3.js
+++ load3.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 45;
+const DB_VERSION = 46;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
