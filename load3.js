9e8a00ee38c2d462b063bc8320444a41cf4f7f4e
b <a>
2021-06-21 13:56:13 -0700

    Update morph set bonus 1.20.3 (real)

--- load3.js
+++ load3.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 56;
+const DB_VERSION = 57;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
