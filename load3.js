d879fa910b6e8a9e8df214890d975e32434c1288
b <a>
2021-06-20 17:16:39 -0700

    Fix arkhalis and other weapons

--- load3.js
+++ load3.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 52;
+const DB_VERSION = 53;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
