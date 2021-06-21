08a74aa4a72a058c337276fec0135f7760d08df2
b <a>
2021-06-21 11:44:20 -0700

    Update items

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 54;
+const DB_VERSION = 55;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
