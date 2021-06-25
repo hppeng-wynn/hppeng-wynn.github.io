8602b06c0ca0cc9ee7e747861648443d6b0fd328
b <a>
2021-06-24 22:56:47 -0700

    Update new chunk

--- load3.js
+++ load3.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 60;
+const DB_VERSION = 61;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
