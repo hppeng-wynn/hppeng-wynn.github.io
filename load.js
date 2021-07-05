ec11b1ffb6e627bcaec31235b56261abc161f5ee
b <a>
2021-07-05 06:50:26 -0700

    Update CIs maybe

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 65;
+const DB_VERSION = 66;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
