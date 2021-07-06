f0ccebd6a9352034dabc4e6108184b5b5a7d94d8
b <a>
2021-07-05 21:33:49 -0700

    Bump db version to force reload

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 67;
+const DB_VERSION = 68;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
