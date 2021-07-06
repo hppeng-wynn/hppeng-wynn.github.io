20326320211f35aab111eea2913638c8dcd01752
b <a>
2021-07-05 22:04:22 -0700

    Fix duplicate entries...

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 69;
+const DB_VERSION = 70;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
