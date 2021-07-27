ff41aae8dff730157b7d43447f8645286c1286a0
b <a>
2021-07-27 03:05:52 -0700

    Bump version

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 75;
+const DB_VERSION = 76;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
