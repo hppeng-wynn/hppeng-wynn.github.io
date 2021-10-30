9ac699afbf4c8e53deefd89adc18dc0790150095
b <a>
2021-10-29 20:13:58 -0700

    WHOOOO API UPDATE

--- js/load_ing.js
+++ js/load_ing.js
@@ -1,4 +1,4 @@
-const ING_DB_VERSION = 9;
+const ING_DB_VERSION = 10;
 
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
