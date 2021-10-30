242230cb670ab109ce7e88cbbcb071122122b831
b <a>
2021-10-30 11:50:42 -0700

    Remember to update compress.json.....

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 83;
+const DB_VERSION = 84;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
