b2c81fa00de596329182a3b2463a9d78cbacb365
b <a>
2021-10-30 04:11:20 -0700

    Fix set bonuses
    Set bonus was entirely broken, all rolled back a long time. Something is wrong with the updating python script -- fix TODO

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 82;
+const DB_VERSION = 83;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
