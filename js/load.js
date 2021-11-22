dc7b3caac0b5a763d7b815eb865b6de6864bb9c2
b <a>
2021-11-22 10:19:20 -0800

    Update ingred json manually... update item json with set bonus for ornament set

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 85;
+const DB_VERSION = 86;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
