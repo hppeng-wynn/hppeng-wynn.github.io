4008dc03ceb9890608798dd541d52da2e96c5238
aspiepuppy <emimeado@gmail.com>
2022-07-10 16:55:24 -0500

    removed phantom ray archetype

--- js/atree_constants.js
+++ js/atree_constants.js
@@ -1882,8 +1882,6 @@ const atrees = {
         {
             "display_name": "Phantom Ray",
             "desc": "Condense Arrow Storm into a single ray that damages enemies 10 times per second",
-            "archetype": "Sharpshooter",
-            "archetype_req": 0,
             "base_abil": "Arrow Storm",
             "parents": ["Water Mastery", "Fire Creep"],
             "dependencies": ["Arrow Storm"],
