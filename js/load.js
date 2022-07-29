08cd090088c86cb6f0af132eddc9efb4dc80928f
hppeng <hppeng>
2022-07-28 22:58:28 -0700

    Remove duplicate relic daggers

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 101;
+const DB_VERSION = 102;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
