03897f676d52657e0109d2467900a1542dbd9698
hppeng <hppeng>
2022-08-11 04:31:52 -0700

    Forgot to bump db version

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 106;
+const DB_VERSION = 107;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
