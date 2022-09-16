a81e936412345e5848f299b18f30b79ada473779
hppeng <hppeng>
2022-09-15 22:42:06 -0700

    HOTFIX: item ID renaming... forgor

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 110;
+const DB_VERSION = 111;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
