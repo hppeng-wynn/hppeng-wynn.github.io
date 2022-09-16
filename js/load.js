33a1e1815419df3e7cb65a5a487396903ebdffaf
hppeng <hppeng>
2022-09-16 11:40:32 -0700

    HOTFIX: frameshift fix
    missing damage fields ffs

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 111;
+const DB_VERSION = 112;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
