50e2360c38bcf11534884b0890752832a9a08e5a
hppeng <hppeng>
2022-09-22 16:50:29 -0700

    Violet shifta
    add a bunch of translations and display commands for new IDs but i definitely missed many of them too... haha fix as we go approach (oops)

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 112;
+const DB_VERSION = 113;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
