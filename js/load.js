73bc5e469e7e4ce86dbd66d65c3131b26fa59a55
hppeng <hppeng>
2022-09-26 05:25:11 -0700

    HOTFIX: Fix frameshift ID
    :skull:

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 114;
+const DB_VERSION = 115;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
