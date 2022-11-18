416a07c78764858c1f732d3b857ef0726dae18a5
hppeng <hppeng>
2022-11-17 22:21:16 -0800

    HOTFIX: patch frameshift (again)
    missing neutral and air damage

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 116;
+const DB_VERSION = 117;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
