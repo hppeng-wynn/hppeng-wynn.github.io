98a309449146427f97760903c61d52e1f31e0391
hppeng <hppeng>
2022-12-16 03:20:49 -0800

    HOTFIX: I hate frameshift

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 118;
+const DB_VERSION = 119;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
