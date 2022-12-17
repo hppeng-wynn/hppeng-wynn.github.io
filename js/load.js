3fd3457fc6696aaf0fc36563133887e944eb3545
hppeng <hppeng>
2022-12-17 09:57:07 -0800

    Bump item db version

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 119;
+const DB_VERSION = 120;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
