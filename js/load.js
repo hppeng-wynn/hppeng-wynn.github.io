88fe5217d20e5980baffa017d74cd8a076892c2b
hppeng-wynn <77039964+hppeng-wynn@users.noreply.github.com>
2023-01-13 21:53:53 -0800

    Manual pruning (2) (#247)
    * manual pruning
    
    woo
    
    * Patch botched find/replace, update other json files and bump version
    
    Co-authored-by: btdmaster1563 <85848265+btdmaster1563@users.noreply.github.com>
    Co-authored-by: hppeng <hppeng>

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 120;
+const DB_VERSION = 121;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
