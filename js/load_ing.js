934b1263fe2e35e67d6f5df3bb059e0ea910bce9
hppeng <hppeng>
2023-04-19 20:29:33 -0700

    Bump ing db version

--- js/load_ing.js
+++ js/load_ing.js
@@ -1,4 +1,4 @@
-const ING_DB_VERSION = 21;
+const ING_DB_VERSION = 22;
 
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
