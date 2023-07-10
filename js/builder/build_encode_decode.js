36ccce1c23deb7979887efb3d3615b3cb689cfdc
hppeng <hppeng>
2023-07-09 23:34:58 -0700

    Bump version to 2.0.3.1
    just tree for now

--- js/builder/build_encode_decode.js
+++ js/builder/build_encode_decode.js
@@ -31,7 +31,8 @@ const wynn_version_names = [
     '2.0.1.1',
     '2.0.1.2',
     '2.0.2.1',
-    '2.0.2.3'
+    '2.0.2.3',
+    '2.0.3.1'
 ];
 const WYNN_VERSION_LATEST = wynn_version_names.length - 1;
 // Default to the newest version.
