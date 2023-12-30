abe6587d986c38a4a1d2597d124c646f9b2997c3
hppeng <hppeng>
2023-12-29 18:01:46 -0800

    Forgot to commit data files, and bump ing db version

--- js/load_ing.js
+++ js/load_ing.js
@@ -1,4 +1,4 @@
-const ING_DB_VERSION = 29;
+const ING_DB_VERSION = 30;
 
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
