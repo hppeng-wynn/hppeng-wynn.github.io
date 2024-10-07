bc5efa94f79255c2ae57b51d15edfad2a9c6404e
hppeng <hppeng>
2024-10-06 22:47:23 -0700

    API update

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 143;
+const DB_VERSION = 144;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
