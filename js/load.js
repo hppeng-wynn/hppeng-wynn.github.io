7afed640451f2a085640b6c5e7259fcc827119ef
hppeng <hppeng>
2024-10-17 16:17:17 -0700

    Morph set buff

--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-const DB_VERSION = 145;
+const DB_VERSION = 146;
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.jsA
 
 let db;
