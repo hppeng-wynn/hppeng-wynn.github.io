dd5cd0d6750f69dc76a1f8f8fdb6b6abf155d18c
davidwnguyen <davidwnguyen@proton.me>
2024-11-12 21:44:52 -0500

    update ingredient DB version

--- js/load_ing.js
+++ js/load_ing.js
@@ -1,4 +1,4 @@
-const ING_DB_VERSION = 35;
+const ING_DB_VERSION = 36;
 
 // @See https://github.com/mdn/learning-area/blob/master/javascript/apis/client-side-storage/indexeddb/video-store/index.js
 
