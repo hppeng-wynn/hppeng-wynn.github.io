7e0bd6f1ee59ab15e14990e1a21db41178e1465a
David Nguyen <davidWnguyen@proton.me>
2025-02-24 17:01:13 -0500

    updated to hotfix 02/24

--- js/load_item.js
+++ js/load_item.js
@@ -213,7 +213,8 @@ const wynn_version_names = [
     '2.1.1.1',
     '2.1.1.2',
     '2.1.1.3',
-    '2.1.1.4'
+    '2.1.1.4',
+    '2.1.1.5'
 ];
 
 const WYNN_VERSION_LATEST = wynn_version_names.length - 1;
